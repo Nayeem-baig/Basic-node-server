034b97b1ef72b661f15546251ac7a4b04425041b
Nayeem-baig <nayeembaig1602@gmail.com>
2022-07-01 18:46:45 +0530

    node

--- openingFile.js
+++ openingFile.js
@@ -6,5 +6,6 @@ fs.open('input.txt', 'r+', function(err, fd) {
    if (err) {
       return console.error(err);
    }
-   console.log("File opened successfully!");     
+   console.log("File opened successfully!");    
+   console.log(__dirname  ); 
 });
